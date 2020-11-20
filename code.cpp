ff7250dcfdea5b3c37f8223fdbc3c762ef70403a
Naman Tayal <ntayalnaman@gmail.com>
2020-11-20 20:40:49 +0530

    implemented vectors instead of arrays

--- code.cpp
+++ code.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #define row 5456
-#define col 4
+#define Xcol 4
+#define Ycol 4
 
 using namespace std;
 
-void read_csv(float X[][col], int Y[])
+void read_csv(vector<vector<float>> &X, vector<vector<int>> &Y)
 {
     fstream fin;
     string data;
@@ -14,21 +16,26 @@ void read_csv(float X[][col], int Y[])
     for (int i = 0; i < row; i++)
     {
         int j;
-        for (j = 0; j < col; j++)
+        for (j = 0; j < Xcol; j++)
         {
             getline(fin, data, ',');
             X[i][j] = stof(data);
         }
+        for (j = 0; j < Ycol - 1; j++)
+        {
+            getline(fin, data, ',');
+            Y[i][j] = stoi(data);
+        }
         getline(fin, data, '\n');
-        Y[i] = stoi(data);
+        Y[i][j] = stoi(data);
     }
     fin.close();
 }
 
 int main()
 {
-    float X[row][col];
-    int Y[row];
+    vector<vector<float>> X(row, vector<float>(Xcol));
+    vector<vector<int>> Y(row, vector<int>(Ycol));
     read_csv(X, Y);
     return 0;
 }
